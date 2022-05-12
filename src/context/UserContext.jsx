415eeec8b9266e0af0fc8375c2f630667c3cafdd
Denzel <dbartolaba123@gmail.com>
2022-05-12 11:19:56 -0700

    created the update portion

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -5,3 +5,46 @@ const initialItems = [
   { id: 1, text: 'Huli huli chicken' },
   { id: 2, text: 'Katsu moco' }
 ];
+
+function foodReducer(food, action) {
+  switch (action.type) {
+    case 'add': {
+      return [
+        ...food,
+        {
+          id: action.id,
+          text: action.text,
+        }
+      ];
+    }
+    case 'edit': {
+      return food.map((foods) => {
+        if (foods.id === action.task.id) {
+          return action.task;
+        }
+        return foods;
+      });
+      } 
+  }
+}
+
+export const useContext = createContext();
+
+export default function ListOfFood({ children }) {
+  const [food, dispatch] = useReducer(foodReducer, initialItems);
+
+  function handleAddItem(text) {
+    dispatch({
+      type: 'add',
+      id: Math.floor(Math.random() * 1000),
+      text
+    });
+  };
+
+  function handleEditItem(task) {
+    dispatch({
+      type: 'edit',
+      task
+    });
+  };
+}
