d857b9b5e53aa4590d9dec7ae03f349d1820874a
Denzel <dbartolaba123@gmail.com>
2022-05-12 11:01:30 -0700

    creating my context for the app

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,7 @@
+import { createContext, useContext, useReducer } from 'react';
+
+const initialItems = [
+  { id: 0, text: 'lomi salmon' },
+  { id: 1, text: 'Huli huli chicken' },
+  { id: 2, text: 'Katsu moco' }
+];
