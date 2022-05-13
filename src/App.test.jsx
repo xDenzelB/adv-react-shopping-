b9ca16e8448b6b9593587a8bb71f3d42efa2098b
Denzel <dbartolaba123@gmail.com>
2022-05-13 12:39:53 -0700

    Test unable to pass doesn't find role of button

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -15,5 +15,15 @@ describe('App', () => {
     expect(food[0]).toBeInTheDocument();
   });
 
+  it('Should update and change the name of the food', async () => {
+    const change = await screen.findByRole('button', { name: 'Edit' });
+    userEvent.click(change[0]);
+
+    const add = await screen.findAllByRole('textbox');
+
+    userEvent.type(add[0], 'Ahi tuna poke');
+    const newFood = await screen.findByText('Ahi tuna poke');
+    expect(newFood).toBeInTheDocument();
+  })
 
 })
