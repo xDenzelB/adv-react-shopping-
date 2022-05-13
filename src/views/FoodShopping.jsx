efb6a9154243170c0cef7a6bcf0282ba5387ca56
Denzel <dbartolaba123@gmail.com>
2022-05-13 10:26:14 -0700

    Added in my addfood on the views, however the text for the food isn't showing up

--- src/views/FoodShopping.jsx
+++ src/views/FoodShopping.jsx
@@ -1,11 +1,13 @@
+import AddFood from "../components/AddFood";
 import FoodList from "../components/FoodList";
 import { useList } from "../context/UserContext";
 
 export default function FoodShopping() {
-  const { food, handleDeleteItem, handleEditItem } = useList();
+  const { food, handleDeleteItem, handleEditItem, handleAddItem } = useList();
 
   return (
     <div>
+      <AddFood newFood={handleAddItem} />
       <FoodList food={food} onDeleteItem={handleDeleteItem} onEditItem={handleEditItem} />
   </div>
 )
